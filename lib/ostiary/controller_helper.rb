ec025c20eec351fb5c4eb8d78476c6451ff8adf4
Jacques Hakvoort <jacques.hakvoort@nedap.com>
2017-01-19 09:08:14 +0100

    Renamed method to :ostiary_policy This better resembles what we’re actually adding. And the name helps you remember the name of the gem used.

--- lib/ostiary/controller_helper.rb
+++ lib/ostiary/controller_helper.rb
@@ -16,6 +16,11 @@ module Ostiary
         @ostiary ||= Ostiary.new
       end
 
+      def required_application_role(role, actions = {})
+        warn "[DEPRECATION] `required_application_role` is deprecated.  Please use `ostiary_policy` instead."
+        ostiary_policy(role, actions)
+      end
+
       # We want to give the option of setting a policy for one action, based on ::Role
       # So we'll copy the way the filters in controllers work.
       # Only apply the role to certain action(s)
@@ -25,7 +30,7 @@ module Ostiary
       # By default a given role will be required for every action
       #
       # One line creates one policy, which are immediately created with the proper class
-      def required_application_role(role, actions = {})
+      def ostiary_policy(role, actions = {})
         if actions.empty?
           self.ostiary.policies << Policy.new(role)
         elsif actions.has_key?(:only)
