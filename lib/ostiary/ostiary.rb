a00454fa8a091d2010911095c71a95d0d27c3382
Jeroen de Jong <jeroen.dejong@nedap.com>
2018-12-27 09:55:38 +0100

    Make block use explicit

--- lib/ostiary/ostiary.rb
+++ lib/ostiary/ostiary.rb
@@ -6,23 +6,23 @@ module Ostiary
       @policies = []
     end
 
-    def authorize!(action)
+    def authorize!(action, &block)
       policies.each do |policy|
-        next if policy_met?(policy, action)
+        next if policy_met?(policy, action, block)
         raise PolicyBroken, policy.error_message(action)
       end
     end
 
-    def authorized?(action)
+    def authorized?(action, &block)
       policies.all? do |policy|
-        policy_met?(policy, action)
+        policy_met?(policy, action, block)
       end
     end
 
     private
 
-    def policy_met?(policy, action)
-      policy.met?(action) { yield(policy.name) }
+    def policy_met?(policy, action, &block)
+      policy.met?(action) { yield policy.name }
     end
 
   end
